884035011cd66fa294a020de154f4fac6a071cd6
Zhou-Shu <54055010+Zhou-Shu@users.noreply.github.com>
2019-09-03 13:24:07 +0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,8 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 from app.models import User
 
+#Format of receiving information on Web pages
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
