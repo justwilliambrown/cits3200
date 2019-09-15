9edcc3af36d11b8eaebfca4fbbe5b9b99f15c2e4
William <justwilliambrown101@gmail.com>
2019-09-15 20:41:40 +0800

    Blackjack server now incorporates betting and interfaces (kinda) with the connection manager.

--- Blackjack_Server.py
+++ Blackjack_Server.py
@@ -1,17 +1,22 @@
 import random
+import ConnMan
 
 # cards: list simulating a deck of cards
 # cardsHeld: 2D list containing the cards held by each player, [0] is the dealer.
 # account: Dictionary for bank account containing the clientID as the key and money remaining as the value
 
 # Receives JSON formatted packet from connection manager
+# {"packet_type" : "GAME", "MOVE" : "HIT/STAND", "bet_amount": eg10, player_id : "fasd"}
 clientIDs = ["1", "2", "3"] # For testing only
-def receive():
-	# TODO
-	return 0
+def receive(gameID, playerID):
+	message = ConnMan.get_game_message(gameID)
+	while message["player_id"] != playerID:
+		message = ConnMan.get_game_message(gameID)
+	return message
 
 def send(data):
 	# TODO
+	ConnMan.send_message(data["player_id"], data)
 	print(data)
 	return 0
 
@@ -44,6 +49,7 @@ def playRound(game_id, roundId, players, account, cards): # Game ID, ID of the r
 	turnId = 0
 	cardsHeld = [] # 2D list, [0] is dealer
 	totals = [] # List of ints [0] is dealer
+	bets = [0]
 
 	cardsHeld.append([])
 	totals.append(0)
@@ -70,15 +76,27 @@ def playRound(game_id, roundId, players, account, cards): # Game ID, ID of the r
 		send({"packet_type": "GAME", "game_id": game_id, "round_id":roundId, "turn_id":turnId, "player_id": players[i], "card" : curCard})
 
 	dealCard1 = cards.pop(0)
+	cards.append(dealCard1)
 	cardsHeld[0].append(dealCard1)
 	totals = calculateTotals(cardsHeld)
+
+	for i in range(1, len(players)):
+		# query the player for a bet amount
+		send({"packet_type" : "CONTROL", "type" : "REQUEST", "item" : "BETAMT", "player_id" : players[i]})
+		message = receive(game_id, player[i])
+		betAmount = message["BETAMT"]
+		bets.append(betAmount)
 
 	for i in range(1, len(players)): #Query each player for a move
 		turnId = 0
 		# query the player
 		#move = "" #TODO
 		print("Player", players[i])
+		send({"packet_type" : "CONTROL", "type" : "REQUEST", "item" : "move", "player_id" : players[i]})
+		message = receive(game_id, player[i])
+		move = message["MOVE"]
 		move = input()
+
 		turnId += 1
 		while move != "STAND":
 			print("Player", players[i], cardsHeld[i])
@@ -87,40 +105,44 @@ def playRound(game_id, roundId, players, account, cards): # Game ID, ID of the r
 				curCard = cards.pop(0)
 				cards.append(curCard)
 				cardsHeld[i].append(curCard)
-				send({"packet_type": "GAME", "game_id": game_id, "round_id":roundId, "turn_id":turnId, "player_id": players[i], "card" : curCard})
+				send({"packet_type": "GAME", "type" : "BROADCAST", "game_id": game_id, "round_id":roundId, "turn_id":turnId, "player_id": players[i], "card" : curCard, "bet_amount" : bets[i]})
 				totals = calculateTotals(cardsHeld)
 				if totals[i] > 21:
 					# Player is out
-					send({"packet_type": "CONTROL", "move":"ELIMINATED", "player_id": players[i]}) # TODO
-					print("Player", players[i], "eliminated", totals[i], cardsHeld[i])
+					send({"packet_type": "CONTROL", "type" : "BROADCAST", "move":"ELIMINATED", "player_id": players[i], "bet_amount":bets[i]}) # TODO
+					account[players[i]] -= bets[i]
 					break
 				# Query player for input
 				move = input()
 				turnId += 1
-		# Send the dealer's second card
-		send({"packet_type": "GAME", "game_id": game_id, "round_id":roundId, "turn_id":turnId, "player_id": 0, "card" : cardsHeld[0][1]})
-		totals = calculateTotals(cardsHeld)
+	# Send the dealer's second card
+	send({"packet_type": "GAME", "type" : "BROADCAST", "game_id": game_id, "round_id":roundId, "turn_id":turnId, "player_id": 0, "card" : cardsHeld[0][1]})
+	totals = calculateTotals(cardsHeld)
 
 	# Dealer needs to hit until soft 17
 	dealcount = 1
 	while totals[0] < 17:
 		dealcount += 1
 		dealCard = cards.pop(0)
+		cards.append(dealCard)
 		cardsHeld[0].append(dealCard)
-		send({"packet_type": "GAME", "game_id": game_id, "round_id":roundId, "turn_id":turnId, "player_id": 0, "card" : cardsHeld[0][dealcount]})
+		send({"packet_type": "GAME", "type" : "BROADCAST", "game_id": game_id, "round_id":roundId, "turn_id":turnId, "player_id": 0, "card" : cardsHeld[0][dealcount]})
 		totals = calculateTotals(cardsHeld)
 
 	# Showdown
 	for i in range(1, len(players)):
 		if totals[i] > 21:
-			break
+			continue
 		elif totals[0] > 21: # Dealer busts, pay out
 			send ({"packet_type": "CONTROL", "move":"WIN", "player_id":players[i]})
+			account[players[i]] += bets[i]
 		else:
 			if totals[i] > totals[0]: # player wins
 				send ({"packet_type": "CONTROL", "move":"WIN", "player_id":players[i]})
+				account[players[i]] += bets[i]
 			else:
 				send ({"packet_type": "CONTROL", "move":"LOSS", "player_id":players[i]})
+				account[players[i]] -= bets[i]
 
 	return(players, account, cards)
 
@@ -134,7 +156,7 @@ def gameStart(game_id, clientIDs):
 	cards = populateDeck()
 
 	for i in range(len(clientIDs)):
-		account[clientIDs[i]] = 100 # 100 Starting balance for now
+		account[clientIDs[i]] = 20 # 100 Starting balance for now
 
 	while len(players) > 1:
 		resultOfRound = playRound(game_id, roundId, players, account, cards)
@@ -145,13 +167,12 @@ def gameStart(game_id, clientIDs):
 		roundId += 1
 
 		playersEliminated = []
+		print("\n", account, "\n")
 		for i in account:
 			if account[i] <= 0:
 				playersEliminated.append(i)
 		for i in playersEliminated:
 			players.remove(i)
-			account.remove(i)
+			account.pop(i)
 
 gameStart(1, [1,2,3])
-print(cards)
-print(len(cards))
