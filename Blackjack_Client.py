f3d755aca1aa6eb11fabaed318d51b5cbc670c7c
LLKanan <54023991+LLKanan@users.noreply.github.com>
2019-10-11 01:41:57 +0800

    New and improved blackjack client

--- Blackjack_Client.py
+++ Blackjack_Client.py
@@ -2,269 +2,258 @@ import random
 import socket
 import threading
 import json
-from time import sleep
 import math
 
-#Global Variables
+#Global variables
 clientID = '-1'
 balance = 0
-betPlace = False
-cardTotal = 0
+cardTotal = {}
 gameID = -1
-
-#Functions:
-#sendJson
-#loginRequest
-#joinQueue
-#gamePacketHandler
-#controlPacketHandler
-#readJson
-#clientLogic
+exit = False
+cardSeen = []
+cardHold = []
+packetQueue = []
+currentBet = 0
 
 #sendJson
 #Input:Socket,Dictionary
-#Outcome:Encodes and sends JSON
+#Outcome: Encodes and sends JSON
 def sendJson(sock,jsonDict):
-	print("SENDING:",jsonDict)
-	sock.send(jsonDict)
+    print("SENDING",jsonDict)
+    sock.send(jsonDict)
 
 #loginRequest
-#Expected outcome: Sends login request to server
+#Outcome: Sends login request to server
+#TODO:Define the packet type for login requests and make sure the server sends a login request
+#In authenticate function of conmann send the packet {"type":"control","subtype":"loginRequest"}
 def loginRequest(sock):
-	username = input("Username:")
-	password = input("Password:")
-	loginDetails = {
-		"Username" : username,
-		"Password" : password
-	}
-	jsonLogin = json.dumps(loginDetails).encode()
-	sendJons(sock,jsonDict)
+    username = input("Username:")
+    password = input("Password:")
+    loginDetails = {
+            "user" : username,
+            "pass" : password,
+    }
+    jsonLogin = json.dumps(loginDetails).encode()
+    sendJson(sock,jsonDict)
 
 #joinQueue
-#Expected outcome: Sends queue request to server
+#Expected outcome:Sends queue request to server
 def joinQueue(sock):
-	qType = input("Queue type: Test,Tournament")
-	while not (qType == "Test" or qType == "Tournament"):
-		print("ERROR: Invalid response")
-		qtype = input("Select Test or Tournament")
-	qDict = {
-		"Queue" : qType
-	}
-	jsonQueue = json.dumps(qDict).encode()
-	sendJson(sock,jsonQueue)
+    qType = input("Queue type: Test , Tournament")
+    while not (qType == "Test" or q Type == "Tournament"):
+        print("Error: Invalid Response")
+        qtype = input("Select 'Test' or 'Tournament'")
+    qDict = {
+            "Queue" : qType
+            "player_id": clientID
+    }
+    jsonQueue = json.dumps(qDict).encode()
+    sendJson(sock,JsonQueue)
 
-#clientLogic
-#Expected outcome:Return Json containing whether or not we wanta nother card or not
+#updateState
+#Expected outcome: Updates the state of the game
+def updateState(cardID,playerID):
+    #If the player hasn't been added to cardTotal add player to cardTotal
+    if playerID not in cardTotal:
+        cardTotal[playerID] = 0
+    #Read the cardID and update the cardTotal for the player accordingly
+    cardSeen.append(cardID)
+    #If it's the client add the card to it's hand
+    if playerID == clientID:
+        cardHold.append(cardID)
+    value = cardID[0]
+    if value == 'J' or value == 'Q' or value = 'K':
+        cardTotal[playerID] += 10
+    if value == '9':
+        cardTotal[playerID] += 9
+    if value == '8':
+        cardTotal[playerID] += 8
+    if value == '7':
+        cardTotal[playerID] += 7
+    if value == '6':
+        cardTotal[playerID] += 6
+    if value == '5':
+        cardTotal[playerID] += 5
+    if value == '4':
+        cardTotal[playerID] += 4
+    if value == '3':
+        cardTotal[playerID] += 3
+    if value == '2':
+        cardTotal[playerID] += 2
+    if value == '1':
+        if cardID[1] == '0':
+            cardTotal[playerID] += 10
+        else:
+            cardTotal[playerID] += 1
 
-def clientLogic(cardID):
-	global cardTotal
-	#Read the cardID(String)
-	#update card total
-	#return dictionary with HIT:True/False,player_id,game_id
-	value = cardID[0]
-	if value == 'J' or value == 'Q' or value == 'K':
-		cardTotal += 10
-	if value == '9':
-		cardTotal += 9
-	if value == '8':
-		cardTotal += 8
-	if value == '7':
-		cardTotal += 7
-	if value == '6':
-		cardTotal += 6
-	if value == '5':
-		cardTotal += 5
-	if value == '4':
-		cardTotal += 4
-	if value == '3':
-		cardTotal += 3
-	if value == '2':
-		cardTotal += 2
-	if value == '1':
-		if cardID[1] == '0':
-			cardTotal += 10
-		else:
-			cardTotal += 1
-	randomElement = random.random()
-	if cardTotal < 17 or randomElement > 0.6:
-		temp = {
-			"packet_type" : "GAME",
-			"game_id" : gameID,
-			"player_id" : clientID,
-			"MOVE" : "HIT",
-			#TESTING - PLACEHOLDER
-			#"BETAMT" : 10,
-			"CONTROL": False
-		}
-	else:
-			temp = {
-			"packet_type" : "GAME",
-			"game_id" : gameID,
-			"player_id" : clientID,
-			"MOVE" : "STAND",
-			#TESTING - PLACEHOLDER
-			#"BETAMT" : 10
-		}
-	return temp
-
-#gamePacketHandler
-#Expected outcome:Reads game JSON and updates values accordingly and sends corresponding packets
+#Game packet handler
+#Function:Handles the game packets
 def gameJsonHandler(jsonDict,sock):
-	global gameID
-	global cardTotal
-	if jsonDict["type"] == "RESET":
-		if jsonDict["game_id"] == gameID:
-			cardTotal = 0
-	elif jsonDict["player_id"] == clientID:
-		if gameID == -1:
-			gameID = jsonDict["game_id"]
-		if not gameID == jsonDict["game_id"]:
-			print("ERROR: Received packet from wrong game - " , jsonDict["game_id"])
-			print("Has correct playerID information,ERROR:Probably server side")
-		else:
-			cardID = jsonDict["card"]
-			clientLogic(cardID)
-			print("CARD TOTAL: ",cardTotal)
-			print("HIT")
-			#if  not cardTotal > 21:
-				#sendJson(sock,hitJson)
+    global gameID
+    if jsonDict["game_id"] == gameID:
+        if jsonDict["type"] == "RESET":
+            cardTotal = 0
+            cardTotal.clear()
+            cardSeen.clear()
+            cardHold.clear()
+        else:
+            cardID = jsonDict["card"]
+            playerID = jsonDict["player_id"]
+            updateState(cardID,playerID)
+    elif jsonDict["game_id"] != gameID:
+        print("ERROR:Received packet from wrong GAMEID: " , jsonDict["game_id"])
+        print("Client's game ID is :",gameID)
 
-	else:
-		#1.Print out other player's moves
-		#This is optional can do it later
-		if gameID == -1:
-			return 0
-		if not gameID == jsonDict["game_id"]:
-			#Error received packet from someone elses game
-			print("ERROR: Received packet from wrong game - " + jsonDict["game_id"])
-
-
-
-#Discuss Integration with ConnMan at a later date
-#controlJsonHandler
+#Control Json Handler
+#Function:Handles the control packets
+#AGENT CODE HERE
 def controlJsonHandler(jsonDict,sock):
-	global clientID
-	global cardTotal
-	#print(jsonDict["subtype"])
-	if clientID == '-1':
-		print("SETUP ID")
-		if jsonDict["subtype"] == "C":
-			clientID = jsonDict["player_id"]
-			print("CLIENTID: " , clientID)
-			#joinQueue(sock)
-	#TODO: Acknowledgement for correct player id
-	elif jsonDict["player_id"] == clientID:
-		if "type" in jsonDict:
-			if jsonDict["type"] == "REQUEST":
-				if jsonDict["item"] == "BETAMT":
-					#print("BET 10")
-					temp = {
-						"packet_type" : "CONTROL",
-						"BETAMT" : 10,
-						"player_id" : clientID,
-						"game_id": gameID
-						}
-					tempDump = json.dumps(temp).encode()
-					sendJson(sock,tempDump)
-					#print("POST BET")
-				if jsonDict["item"] == "move":
-					if cardTotal < 17:
-						temp = {
-							"packet_type" : "GAME",
-							"MOVE" : "HIT",
-							"player_id" : clientID,
-							"game_id":gameID,
-							"CONTROL": True
-							}
-						tempDump = json.dumps(temp).encode()
-						sendJson(sock,tempDump)
-						print("CARD TOTAL: ",cardTotal)
-						print("HIT")
-					else:
-						temp = {
-							"packet_type" : "GAME",
-							"MOVE" : "STAND",
-							"player_id" : clientID,
-							"game_id":gameID
-							}
-						tempDump = json.dumps(temp).encode()
-						sendJson(sock,tempDump)
-						print("CARD TOTAL: ",cardTotal)
-						print("STAND")
+    global clientID
+    global cardTotal
+    if clientID == '-1':
+        #Initialising login 
+        if jsonDict["subtype"] == "loginRequest":
+            loginRequest(sock)
+        #Now assigning the clientID
+        elif jsonDict["subtype"] == "C":
+            clientID = jsonDict["player_id"]
+            print("CLIENTID : ",clientID)
+            #Now pick a queue type to join
+            joinQueue(sock)
+    elif jsonDict["player_id"] == clientID:
+        if "subtype" in jsonDict:
+            if["subtype"] == "DC":
+                exit = True
+                break
 
+        if "type" in jsonDict:
+            if jsonDict["type"] = "OPENING_BALANCE":
+                gameID = jsonDict["game_id"]
+                balance = jsonDict["BALANCE"]
+                cardTotal = 0
+                cardTotal.clear()
+                cardSeen.clear()
+                cardHold.clear()
+            else if jsonDict["type"] = "LOBBY":
+                print("CONGRATULATIONS YOU'VE WON")
+                print("You'll now be placed into the lobby for your next match")
+            if gameID = jsonDict["game_id"]
+                #-------------------------------------------------------------------
+                #AGENT LOGIC HERE
+                if jsonDict["type"] = "REQUEST":
+                    if jsonDict["item"] = "BETAMT":
+                        currentBet = 10
+                        temp = {
+                                "packet_type" : "CONTROL",
+                                "BETAMT" : currentBet,
+                                "player_id" : clientID,
+                                "game_id": gameID
+                                }
+                        tempDump.json.dumps(temp).encode()
+                        sendJson(sock,tempDump)
+                    if jsonDict["item"] == "move":
+                        if cardTotal < 17:
+                            temp = {
+                                    "packet_type" : "GAME",
+                                    "MOVE": "HIT"
+                                    "player_id" : clientID,
+                                    "game_id" : gameID,
+                                    "CONTORL": True
+                                    }
+                            tempDump = json.dumps(temp).encode()
+                            sendJson(sock,tempDump)
+                            print("CARD TOTAL: ",cardTotal[clientID])
+                            print("HIT")
+                        else:
+                            temp = {
+                                    "packet_type" : "GAME",
+                                    "MOVE" : "STAND",
+                                    "player_id" : clientID,
+                                    "game_id":gameID
+                                    }
+                            tempDump = json.dumps(temp).encode()
+                            sendJson(sock,tempDump)
+                            print("CARD TOTAL: ",cardTotal)
+                            print("STAND")
+                #----------------------------------------------------------------------
+                elif jsonDict["type"] = "BROADCAST":
+                    if move in jsonDict:
+                        if jsonDict["move"] == "ELIMINATED" || jsonDict["move"] == "LOSS":
+                            balance -= currentBet
+                            currentBet = 0
+                            if balance = 0:
+                                print("Balance is 0 you've lost")
+                                exit = True
+                        elif jsonDict["move"] == "WIN":
+                            balance += currentBet
+                            currentBet = 0
+                else if jsonDict["type"] == "GAME_LOSS":
+                    print("You've lost")
+                    exit = True
+                else if jsonDict["type"] == "VICTORY":
+                    print("Congratulations you've won")
+                    exit = True
 
-#readJson
-#Expected outcome:Determines what type of json packet is and runs the appropriate function
+#Read Json
+#Function:Determins what type of json packet it is and checks if it's a valid gameid/playerid
 def readJson(jsonDict,sock):
-	#If this doesn't work use
-	#json.loads(jsonDict)[0]
-	print("NEW JSON : ",jsonDict)
-	if jsonDict["packet_type"] == "CONTROL":
-		controlJsonHandler(jsonDict,sock)
-	elif jsonDict["packet_type"] == "GAME":
-		gameJsonHandler(jsonDict,sock)
+    print("NEW JSON: ",jsonDict)
+    if jsonDict["packet_type"] == "CONTROL":
+        controlJsonHandler(jsonDict,sock)
+    elif jsonDict["packet_type"] == "GAME":
+        gameJsonHandler(jsonDict,sock)
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 3001)
-print ('connecting to %s port %s' % server_address)
-sock.connect(server_address)
-
-#loginRequest(sock)
-#joinQueue(sock)
-
-#function for handling the packets in the queue
-packetQueue = []
+#Packet Queue Handler
+#Function: Thread to process the packets in the queue
 def packetQueueHandler():
-	while True:
-		#print("LENGTH :" ,len(packetQueue))
-		if len(packetQueue) != 0:
-			tempPacket = packetQueue.pop(0)
-			readJson(tempPacket,sock)
-		if exit:
-			print("Exit is true\n")
-			break
-first = True
-#Socket Loop
-try:
-	while True:
-		exit = False
-		# Loop for packet reading
-		message = sock.recv(2048)
-		amount_received = 0
-		amount_expected = len(message)
+    while True:
+        if len(packetQueue) != 0:
+            tempPacket = packetQueue.pop(0)
+            print("PACKET RECEIVED: ",tempPacket)
+            readJson(tempPacket,sock)
+        if exit:
+            print("Exiting packet Queue Thread\n")
+            break
 
-		while amount_received < amount_expected:
-			#data = sock.recv(1024)
-			amount_received += len(message)
-			packet = message.decode()
-			print("PRE JSON LOADS(PACKET): " , packet)
-			packetCount = packet.count("{")
-			if(packetCount > 1):
-				packetSplit = packet.split("}",packetCount - 1)
-				for temp in packetSplit:
-					if(temp[len(temp) - 1] != '}'):
-						load = temp + '}'
-					else:
-						load = temp
-					print("LOAD: ",load)
-					packetJson = json.loads(load)
-					print("POST JSON LOADS(PACKET): ", load)
-					packetQueue.append(packetJson)
-			else:
-				packetJson = json.loads(packet)
-				print("POST JSON LOADS(PACKET): ", packetJson)
-				packetQueue.append(packetJson)
-		if first == True:
-			first = False
-			pQHandler = threading.Thread(target = packetQueueHandler)
-			pQHandler.start()
-			
-		if exit:
-			print("Exit is true\n")
-			break
+#Socket Connection 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost',3001)
+print('Connecting to %s port %s' %server_address)
+sock.connect(server_address)
+first = True
+try:
+    while True:
+        if first == True:
+            first = False
+            pQHandler = threading.Thread(target=packetQueueHandler)
+            pqHandler.start()
+        message = sock.recv(4096)
+        amount_received = 0
+        amount_expected = len(message)
+
+        while amount_received < amount expected:
+            amount_received += len(message)
+            packet = message.decode()
+            #print("PRE JSON LOADS(PACKET): ",packet)
+            packetCount = packet.count("{")
+            if(packetCount > 1):
+                packetSplit = packet.split("}",packetCount -1)
+                for temp in packetSplit:
+                    if(temp[len(temp) -1] != '}'):
+                        load = temp + '}'
+                    else:
+                        load = temp
+                    #print("LOAD: ",load)
+                    packetJson = json.loads(load)
+                    #print("POST JSON LOADS(PACKET): ",load)
+                    packetQueue.append(packetJson)
+            else:
+                packetJson = json.loads(packet)
+                #print("POST JSON LOADS(PACKET): ",packetJson)
+                packetQueue.append(packetJson)
+        if exit:
+            print("EXITING PROGRAM\n")
+            break
 finally:
-	print ('closing socket')
-	sock.close()
+    print("Closing socket")
+    sock.close()
