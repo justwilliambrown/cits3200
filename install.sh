6091a31f633bafc4c15c0b22ecc0a2fc297e840d
tactical_fluke <tacticalfluke304@gmail.com>
2019-10-10 13:17:22 +0800

    deleted connTest_client as it was no longer needed

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb 
 sudo dpkg -i mysql-apt-config_0.8.13-1_all.deb 
 sudo apt-get update 
-sudo apt install mysql-server 
+sudo apt-get install mysql-server 
 #install python3 and utilities
-sudo apt install python3 
-sudo apt install python3-venv python3-pip 
+sudo apt-get install python3 
+sudo apt-get install python3-venv 
+sudo apt-get install python3-pip 
 #create a virtual environment for all our dependencies
 python3 -m venv venv 
 #installing all the dependencies for the web front end
