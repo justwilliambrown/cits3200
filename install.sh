22aa9626592f01cd323213b7ee10bdebb80e2df3
tactical_fluke <tacticalfluke304@gmail.com>
2019-10-11 10:37:18 +0800

    updating install script

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#download and install the mysql server
+#download the mysql server
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb 
 #install python3 and utilities
-sudo apt-get install python3 
-sudo apt-get install python3-venv 
-sudo apt-get install python3-pip 
+echo installing python 3 and libraries
+sudo apt-get install python3 python3-venv python3-pip
 #create a virtual environment for all our dependencies
 python3 -m venv venv 
 #installing all the dependencies for the web front end
