2367416bb586994cf1dc121a23f593f25fb56d01
tactical_fluke <tacticalfluke304@gmail.com>
2019-10-12 20:16:15 +0800

    added initial (untested) C library

---
+++ libraries/Colosseum.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <socket.h>
+#include <cjson/cJSON.h>
+
+int connect(char* host, int hostlen, int port, char* user, int userlen, char* pass, int passlen);
+void beginPlay();
+
+int registerBroadcast(void (*callback)( const cJSON&, cJSON*));
+int registerRequest(void (*callback)(const cJSON&, cJSON*));
+int registerReset(void (*callback));
