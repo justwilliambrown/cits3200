f676329fd2ce49fae3c54f902a86bf567b8716e3
Aidan Haile <tacticalfluke304@gmail.com>
2019-10-13 20:11:40 +0800

    fixed up install scripts a little

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb 
 #install python3 and utilities
 echo installing python 3 and libraries
-sudo apt-get install python3 python3-venv python3-pip
+sudo apt-get -y update
+sudo apt-get -y install python3 python3-venv python3-pip
 #create a virtual environment for all our dependencies
 python3 -m venv venv 
 #installing all the dependencies for the web front end
--- setupsql.sh
+++ setupsql.sh
@@ -1,5 +1,5 @@
 echo "create user '$2'@'%' identified by password('$3');"| mysql --user=root --password=$1
-echo "create database app; select app"| mysql -user=root --password=$1
+echo "create database app; select app;"| mysql --user=root --password=$1
 mkdir upload_files
 mkdir gamelogs
 flask db init
