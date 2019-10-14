7f607c24fa7a2027523cb12c61896cf7c39da4ba
Aidan Haile <tacticalfluke304@gmail.com>
2019-10-14 15:42:43 +0000

    further work on install

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash 
 #download the mysql server
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb 
 #install python3 and utilities
 echo installing python 3 and libraries
-sudo apt-get -y update
-sudo apt-get -y install python3 python3-venv python3-pip
+sudo apt-get -y update 
+sudo apt-get -y install python3 python3-venv python3-pip 
 #create a virtual environment for all our dependencies
 python3 -m venv venv 
 #installing all the dependencies for the web front end
-python3 -m pip install -r dependencies.txt
+python3 -m pip install -r dependencies.txt 
--- setupsql.sh
+++ setupsql.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-mysql_config_editor set --login-path=local --host=localhost --user=root --password
-mysql --login-path=local -e "create user '$1'@'%' identified by '$2';"
-mysql --login-path=local -e "create database app; select app;"
-mkdir upload_files
-mkdir gamelogs
-flask db init
-flask db migrate -m "users table"
-flask db upgrade
-cat > sqluser <<- END
+mysql_config_editor set --login-path=local --host=localhost --user=root --password 
+mysql --login-path=local -e "create user '$1'@'%' identified by '$2';" 
+mysql --login-path=local -e "create database app; select app;" 
+mkdir upload_files 
+mkdir gamelogs 
+flask db init 
+flask db migrate -m "users table" 
+flask db upgrade 
+cat > sqluser <<- END 
 {
 	"user" : "$1",
 	"password" : "$2"
