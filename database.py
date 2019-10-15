cecbc5c26f1cdf06d56dafc3aa3af6d1a1e5a1b4
LLKanan <54023991+LLKanan@users.noreply.github.com>
2019-10-15 12:39:53 +0800

    added global db

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ import json
 db = None
 
 def setup_db():
+	global db
 	file = open("sqluser.txt", 'r')
 	userinfo = file.read()
 	userdict = json.loads(userinfo)
@@ -11,20 +12,23 @@ def setup_db():
 	file.close()
 
 def getMMR(clientID):
-    dbCursor = db.cursor(prepared=True)
-    #TODO: Check that these are the column names in the database
-    statement = "SELECT ranking FROM User Where id = %s"
-    dbCursor.execute(query,clientID)
-    if ranking in dbCursor:
-        return ranking
-    else:
-        return -1
+	global db
+	dbCursor = db.cursor(prepared=True)
+	#TODO: Check that these are the column names in the database
+	statement = "SELECT ranking FROM User Where id = %s"
+	dbCursor.execute(query,clientID)
+	if ranking in dbCursor:
+		return ranking
+	else:
+		return -1
 
 def updateMMR(clientID,newMMR):
-    dbCursor = db.cursor(prepared = True)
-    statement = "UPDATE user SET ranking = %s Where id = %s"
-    data = (newMMR,clientID)
-    dbCursor.execute(statement,data)
+	global db
+	dbCursor = db.cursor(prepared = True)
+	statement = "UPDATE user SET ranking = %s Where id = %s"
+	data = (newMMR,clientID)
+	dbCursor.execute(statement,data)
 
 def getDB():
+	global db
 	return db
