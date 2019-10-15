6142b70f83e6092e191cfee4c0408fbde8ac833c
Zhou-Shu <54055010+Zhou-Shu@users.noreply.github.com>
2019-10-15 12:28:23 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -55,7 +55,7 @@ def register():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
+        user = User(username=form.username.data, email=form.email.data, ranking=1000)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
