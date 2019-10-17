e6c23f976ef476e52e540c631d26d9191092c605
Aidan Haile <tacticalfluke304@gmail.com>
2019-10-17 10:59:16 +0800

    adding installation instructions and doing script work

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash 
 #download the mysql server
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb 
-#install python3 and utilities
-echo installing python 3 and libraries
-sudo apt-get -y update 
-sudo apt-get -y install python3 python3-venv python3-pip 
-#create a virtual environment for all our dependencies
-python3 -m venv venv 
+wget -O mysql.deb https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb 
 #installing all the dependencies for the web front end
 python3 -m pip install -r dependencies.txt 
+mkdir gamelogs upload_files cfg
--- setupsql.sh
+++ setupsql.sh
@@ -1,15 +1,11 @@
 #! /bin/bash
-mysql_config_editor set --login-path=local --host=localhost --user=root --password 
 mysql --login-path=local -e "create user '$1'@'%' identified by '$2';" 
-mysql --login-path=local -e "create database app; select app;" 
-mkdir upload_files 
-mkdir gamelogs 
-flask db init 
-flask db migrate -m "users table" 
-flask db upgrade 
+mysql --login-path=local -e "create database app; select app;"
+mysql --login-path=local -e "grant all privileges on app.* to '$1'@'%';"
 cat > sqluser <<- END 
 {
 	"user" : "$1",
 	"password" : "$2"
 }
 END
+echo "$1:$2@%" | sudo cat >> /etc/environment
