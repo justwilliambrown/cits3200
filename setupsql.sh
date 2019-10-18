2df48451a2b45f54b96565384120b3ce00cace1a
tactical-fluke <tacticalfluke304@gmail.com>
2019-10-18 11:01:24 +0800

    Update setupsql.sh

--- setupsql.sh
+++ setupsql.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 mysql --login-path=local -e "create user '$1'@'%' identified by '$2';" 
 mysql --login-path=local -e "create database app; select app;"
-mysql --login-path=local -e "grant all privileges on app.* to '$1'@'%';"
+mysql --login-path=local -e "grant all privileges on app.* to $1;"
 cat > sqluser.txt <<- END 
 {
 	"user" : "$1",
 	"password" : "$2"
 }
 END
-echo "SQLUSER=$1:$2@%" | sudo cat >> /etc/environment
+echo "SQL_USER=$1:$2@%" | sudo cat >> /etc/environment
