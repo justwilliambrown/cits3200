54a0ceeae9b0c11172caf6cbbf7d42483af76159
tactical_fluke <tacticalfluke304@gmail.com>
2019-10-19 23:20:17 +0800

    installation work

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 #download the mysql server
 wget -O mysql.deb https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb 
 #installing all the dependencies for the web front end
+python3 -m pip install --upgrade pip
 python3 -m pip install -r dependencies.txt 
-mkdir gamelogs upload_files cfg
+mkdir gamelogs upload_files cfg 
--- setupsql.sh
+++ setupsql.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-mysql --login-path=local -e "create user '$1'@'%' identified by '$2';" 
+mysql --login-path=local -e "create user '$1'@'%' identified by '$2';"
 mysql --login-path=local -e "create database app; select app;"
 mysql --login-path=local -e "grant all privileges on app.* to $1;"
-cat > sqluser.txt <<- END 
+cat > sqluser.txt <<- END
 {
 	"user" : "$1",
 	"password" : "$2"
 }
 END
-echo "SQL_USER=$1:$2@%" | sudo cat >> /etc/environment
+#echo "SQL_USER=$1:$2@%" | sudo cat >> /etc/environment
+c=`cat <<EOF
+import os
+filepath = os.getcwd() + "/config.py"
+file = open(filepath, "r")
+f = file.read()
+file.close()
+f = f.replace("REPLACEME", "$1:$2@localhost")
+file = open(filepath, "w+")
+file.write(f)
+file.close()
+EOF`
+python -c "$c"
