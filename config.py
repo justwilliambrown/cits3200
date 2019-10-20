7db647892507078d8573922a6ebd6bc08068d8a0
tactical-fluke <tacticalfluke304@gmail.com>
2019-10-20 17:41:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{0}:3306/app'.format(os.environ.get('SQL_USER'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADED_FILES_DEST = os.getcwd()+"/userSource"
+    UPLOADED_FILES_DEST = os.getcwd()
     UPLOADED_FILES_ALLOW = ('py', 'java','js','c','cpp')
     LOG_FILES_DEST = os.getcwd()+"/gamelogs"
