a9cf05bc9a224079b18fe8dbcd1934ad694da3da
tactical-fluke <tacticalfluke304@gmail.com>
2019-10-21 12:46:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{0}:3306/app'.format(os.environ.get('SQL_USER'))
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://REPLACEME:3306/app'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_FILES_DEST = os.getcwd()
     UPLOADED_FILES_ALLOW = ('py', 'java','js','c','cpp')
