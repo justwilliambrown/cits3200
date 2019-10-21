b03e79dde576a80b8b47e5da633af28bcf5d3243
Aidan Haile <tacticalfluke304@gmail.com>
2019-10-21 06:39:33 +0000

    database.py fix

--- database.py
+++ database.py
@@ -1,18 +1,21 @@
 import mysql.connector
 import json
+import threading
 
 db = None
+dbLock = threading.Lock()
 
 def setup_db():
 	global db
 	file = open("sqluser.txt", 'r')
 	userinfo = file.read()
 	userdict = json.loads(userinfo)
-	db = mysql.connector.connect(host='localhost', user=userdict['user'], password=userdict['password'],database='app')
+	db = mysql.connector.connect(host='localhost', user="root", password="a",database='app')
 	file.close()
 
 def getMMR(clientID):
 	global db
+	dbLock.acquire()
 	dbCursor = db.cursor(prepared=True)
 	statement = "SELECT ranking,id FROM user Where id = %s"
 	dbCursor.execute(statement,(clientID,))
@@ -23,16 +26,33 @@ def getMMR(clientID):
 		#print("RANKING: ",ranking)
 		rank = ranking
 	dbCursor.close()
+	dbLock.release()
 	return rank
 
 def updateMMR(clientID,newMMR):
+	dbLock.acquire()
 	global db
 	dbCursor = db.cursor(prepared = True)
 	statement = "UPDATE user SET ranking = %s Where id = %s"
 	#data = (newMMR,clientID)
 	dbCursor.execute(statement,(newMMR,clientID))
 	db.commit()
+	dbLock.release()
 	dbCursor.close()
+
+def getUsers(username):
+	dbLock.acquire()
+	global db
+	cursor = db.cursor(prepared=True)
+	stmt = "select id, username, password_hash, ranking from user where username = %s"
+	cursor.execute(stmt, (username,))
+	retlist = []
+	for (id, username, pHash, rank) in cursor:
+		retlist.append((id, username, pHash, rank))
+
+	cursor.close()
+	dbLock.release()
+	return retlist
 
 def getDB():
 	global db
