86d04fa4c323d154d5b66a27c35dc6d7ca16d747
Aidan Haile <tacticalfluke304@gmail.com>
2019-10-21 08:04:32 +0000

    final install stuff

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@
 wget -O mysql.deb https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb 
 #installing all the dependencies for the web front end
 python3 -m pip install --upgrade pip
-python3 -m pip install --user -r dependencies.txt
-mkdir gamelogs upload_files cfg 
+mkdir gamelogs cfg upload_files 
